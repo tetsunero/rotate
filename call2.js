ec3574722dbaea3e81fac8858e9698b21c3d9c14
tetsunero <dedy.iswana2@gmail.com>
2018-01-24 00:31:46 +0700

    Update call2.js

--- call2.js
+++ call2.js
@@ -21,11 +21,11 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="2018/01/samsung-galaxy-j2-pro-2018-harga-dan_9.html"}
-if(x=="2"){xxx="2018/01/samsung-galaxy-a8-2018-harga-dan_2.html"}
-if(x=="3"){xxx="2018/01/samsung-galaxy-a8-2018-harga-dan_2.html"}
-if(x=="4"){xxx="2018/01/samsung-galaxy-a8-2018-harga-dan_2.html"}
-if(x=="5"){xxx="2018/01/samsung-galaxy-a8-2018-harga-dan_2.html"}a_to_ve[i].href="https://myfilephone.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="samsung-galaxy-j2-pro-2018-harga-dan_9.html"}
+if(x=="2"){xxx="samsung-galaxy-a8-2018-harga-dan_2.html"}
+if(x=="3"){xxx="samsung-galaxy-a8-2018-harga-dan_2.html"}
+if(x=="4"){xxx="samsung-galaxy-a8-2018-harga-dan_2.html"}
+if(x=="5"){xxx="samsung-galaxy-a8-2018-harga-dan_2.html"}a_to_ve[i].href="https://myfilephone.blogspot.com/2018/01/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
