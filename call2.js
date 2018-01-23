dbfc245706f358f61ec00600f72203fd26a189a9
tetsunero <dedy.iswana2@gmail.com>
2018-01-24 00:14:10 +0700

    Update call2.js

--- call2.js
+++ call2.js
@@ -22,12 +22,12 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="/2018/01/samsung-galaxy-j2-pro-2018-harga-dan_9.html"}
-if(x=="2"){xxx="/2017/12/samsung-galaxy-s9-harga-dan-spesifikasi_50.html"}
-if(x=="3"){xxx="/2017/12/samsung-galaxy-s9-harga-dan-spesifikasi_50.html"}
-if(x=="4"){xxx="/2017/12/samsung-galaxy-s9-harga-dan-spesifikasi_50.html"}
-if(x=="5"){xxx="/2017/12/samsung-galaxy-s9-harga-dan-spesifikasi_50.html"}
-a_to_ve[i].href="myfilephone.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="2018/01/samsung-galaxy-j2-pro-2018-harga-dan_9.html"}
+if(x=="2"){xxx="2018/01/samsung-galaxy-a8-2018-harga-dan_2.html"}
+if(x=="3"){xxx="2018/01/samsung-galaxy-a8-2018-harga-dan_2.html"}
+if(x=="4"){xxx="2018/01/samsung-galaxy-a8-2018-harga-dan_2.html"}
+if(x=="5"){xxx="2018/01/samsung-galaxy-a8-2018-harga-dan_2.html"}
+a_to_ve[i].href="https://myfilephone.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
