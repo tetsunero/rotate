c317c4b114e7b84af90390e56872187741ba1cb5
tetsunero <dedy.iswana2@gmail.com>
2018-01-24 00:19:34 +0700

    Update call2.js

--- call2.js
+++ call2.js
@@ -1,4 +1,3 @@
-
 var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(input){var output="";var chr1,chr2,chr3,enc1,enc2,enc3,enc4;var i=0;input=Base64._utf8_encode(input);while(i<input.length){chr1=input.charCodeAt(i++);chr2=input.charCodeAt(i++);chr3=input.charCodeAt(i++);enc1=chr1>>2;enc2=((chr1&3)<<4)|(chr2>>4);enc3=((chr2&15)<<2)|(chr3>>6);enc4=chr3&63;if(isNaN(chr2)){enc3=enc4=64;}else if(isNaN(chr3)){enc4=64;}
 output=output+ this._keyStr.charAt(enc1)+ this._keyStr.charAt(enc2)+ this._keyStr.charAt(enc3)+ this._keyStr.charAt(enc4);}
 return output;},decode:function(input){var output="";var chr1,chr2,chr3;var enc1,enc2,enc3,enc4;var i=0;input=input.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(i<input.length){enc1=this._keyStr.indexOf(input.charAt(i++));enc2=this._keyStr.indexOf(input.charAt(i++));enc3=this._keyStr.indexOf(input.charAt(i++));enc4=this._keyStr.indexOf(input.charAt(i++));chr1=(enc1<<2)|(enc2>>4);chr2=((enc2&15)<<4)|(enc3>>2);chr3=((enc3&3)<<6)|enc4;output=output+ String.fromCharCode(chr1);if(enc3!=64){output=output+ String.fromCharCode(chr2);}
@@ -26,8 +25,7 @@ if(x=="1"){xxx="2018/01/samsung-galaxy-j2-pro-2018-harga-dan_9.html"}
 if(x=="2"){xxx="2018/01/samsung-galaxy-a8-2018-harga-dan_2.html"}
 if(x=="3"){xxx="2018/01/samsung-galaxy-a8-2018-harga-dan_2.html"}
 if(x=="4"){xxx="2018/01/samsung-galaxy-a8-2018-harga-dan_2.html"}
-if(x=="5"){xxx="2018/01/samsung-galaxy-a8-2018-harga-dan_2.html"}
-a_to_ve[i].href="https://myfilephone.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="2018/01/samsung-galaxy-a8-2018-harga-dan_2.html"}a_to_ve[i].href="https://myfilephone.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
